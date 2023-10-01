eef71ae92c90ca8a4766085c803d45aa384c4c95
Debnath <dtanmoy@netapp.com>
2023-10-01 14:19:07 +0530

    updated workflow file 8

--- src/app.py
+++ src/app.py
@@ -1,22 +1,22 @@
-# import sys, os
-# from datetime import datetime
-# def    greet_user(name):
-#     print("Hello, " + name + "!")
-# greet_user("Tanmoy")
-# current_time=datetime.now()
-# print("Current time is " + str(current_time))
-
-
-"""
-Sample code to test pylint
-
-"""
+import sys, os
 from datetime import datetime
-def greet_user(name):
-    """
-        Greets an user
-    """
-    return "Hello, " + name + "!"
-print(greet_user("Tanmoy"))
+def    greet_user(name):
+    print("Hello, " + name + "!")
+greet_user("Tanmoy")
 current_time=datetime.now()
 print("Current time is " + str(current_time))
+
+
+# """
+# Sample code to test pylint
+#
+# """
+# from datetime import datetime
+# def greet_user(name):
+#     """
+#         Greets an user
+#     """
+#     return "Hello, " + name + "!"
+# print(greet_user("Tanmoy"))
+# current_time=datetime.now()
+# print("Current time is " + str(current_time))
--- tests/app_test.py
+++ tests/app_test.py
@@ -3,11 +3,11 @@ test for app module
 
 """
 
-# from ..src import app
-#
-#
-# def test_greet_user():
-#     """
-#     TEST greet use function
-#     """
-#     assert app.greet_user("Tanmoy") == "Hello, Tanmoy!"
+from ..src import app
+
+
+def test_greet_user():
+    """
+    TEST greet use function
+    """
+    assert app.greet_user("Tanmoy") == "Hello, Tanmoy!"
